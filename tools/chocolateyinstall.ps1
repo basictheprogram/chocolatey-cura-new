07dc9b03314a4dd22ed40f38d0bfce5e632ffe51
Bob Tanner <tanner@real-time.com>
2019-08-02 11:57:31 -0500

    Close #4   Release cura-new 4.2.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,25 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-# https://download.ultimaker.com/cura/Ultimaker_Cura-4.1.0-win64.exe
+# https://download.ultimaker.com/cura/Ultimaker_Cura-4.2.1-win64.exe
 
-$FullPackage = "Ultimaker_Cura-4.1.0-win64.exe"
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://download.ultimaker.com/cura/' + $FullPackage
-$WorkSpace   = Join-Path $env:TEMP $env:ChocolateyPackageName 
+$FullPackage = "Ultimaker_Cura-4.2.1-win64.exe"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://download.ultimaker.com/cura/' + $FullPackage
+$WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName 
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url64bit      = $url64
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url64bit       = $url64
 
-  softwareName  = 'Ultimakercura*'
+    softwareName   = 'Ultimakercura*'
 
-  checksum64    = '1c2b2deaec0a3f56242ebbeeb56410ed0640fa5bbd91bbb7e78399592e206ef3'
-  checksumType64= 'sha256'
+    checksum64     = 'b463873df49434f2dbc960746a92a0767fe0477a80bf86903624f4b51cf4e711'
+    checksumType64 = 'sha256'
 
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs   = '/S'
+    validExitCodes = @(0, 3010, 1641)
+    silentArgs     = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
