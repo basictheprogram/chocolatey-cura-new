0d3bea4f70f5447124fb80d991f1c33401def305
Bob Tanner <tanner@real-time.com>
2019-12-29 11:04:18 -0600

    New release v4.4.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-# https://github.com/Ultimaker/Cura/releases/download/v4.4.0/Ultimaker.Cura-4.4.0-win64.exe
+# https://github.com/Ultimaker/Cura/releases/download/v4.4.1/Ultimaker.Cura-4.4.1-win64.exe
 
-$FullPackage = "Ultimaker.Cura-4.4.0-win64.exe"
+$FullPackage = "Ultimaker.Cura-4.4.1-win64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/v4.4.0/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/v4.4.1/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName 
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
 
     softwareName   = 'Ultimakercura*'
 
-    checksum64     = '0a7109a64e0938a77d9a987ef0ae6867d4e98112420642633461598fbd6fa00a'
+    checksum64     = '7f72556e5ce55faf0626757968ffa122cd91df255852c84c908383c9b6ffb0aa'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
@@ -23,4 +23,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
