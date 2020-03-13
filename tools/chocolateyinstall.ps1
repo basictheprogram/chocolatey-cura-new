2dd4bdc3ab0ab05fdc495de2dd48973a77a0743c
Bob Tanner <tanner@real-time.com>
2020-03-13 17:41:41 -0500

    New release v4.5.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-
-# https://github.com/Ultimaker/Cura/releases/download/4.4.1/Ultimaker.Cura-4.4.1-win64.exe
-
-$FullPackage = "Ultimaker.Cura-4.4.1-win64.exe"
+$FullPackage = "Ultimaker.Cura-4.5.0-win64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.4.1/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.5.0/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName 
 
 $packageArgs = @{
@@ -15,7 +12,7 @@ $packageArgs = @{
 
     softwareName   = 'Ultimakercura*'
 
-    checksum64     = '7f72556e5ce55faf0626757968ffa122cd91df255852c84c908383c9b6ffb0aa'
+    checksum64     = 'ff9afb3b969524f9da472be91eda137ec0c0bc5f59d30183fd8f260bf6fbf0d8'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
@@ -23,3 +20,5 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
+
+# https://github.com/Ultimaker/Cura/releases/download/4.5.0/Ultimaker.Cura-4.5.0-win64.exe
