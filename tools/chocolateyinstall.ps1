b2f307b3bbc27c75e7ff9bd59db9f09f5497f7fa
Bob Tanner <tanner@real-time.com>
2020-11-17 14:19:22 -0600

    New release cura-new v4.8.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,24 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
-$FullPackage = "Ultimaker_Cura-4.7.0-amd64.exe"
+$FullPackage = "Ultimaker_Cura-4.8.0-amd64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.7.0/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.8/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
-  fileType       = 'exe'
-  url64bit       = $url64
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url64bit       = $url64
 
-  softwareName   = 'Ultimakercura*'
+    softwareName   = 'Ultimakercura*'
 
-  checksum64     = '5d7ae8cd670ebef325a5353d15d1cbca3a88934aa98f35d4c21c0eea8d5487d2'
-  checksumType64 = 'sha256'
+    checksum64     = '3823b2132472ff7906b32c9763cf1fb9cf436726534ad9c2d0b8e93916433f3d'
+    checksumType64 = 'sha256'
 
-  validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'
+    validExitCodes = @(0, 3010, 1641)
+    silentArgs     = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
 
-# https://github.com/Ultimaker/Cura/releases/download/4.6.1/Ultimaker.Cura-4.6.1-win64.exe
+# https://github.com/Ultimaker/Cura/releases/download/4.8/Ultimaker_Cura-4.8.0-amd64.exe
