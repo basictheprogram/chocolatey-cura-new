f499d8ee7dc67e2b73ebc12e84b7ff4ad5684ba0
Bob Tanner <tanner@real-time.com>
2021-05-15 13:40:43 -0500

    New release cura-new v4.9.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-$FullPackage = "Ultimaker_Cura-4.9.0-amd64.exe"
+$FullPackage = "Ultimaker_Cura-4.9.1-amd64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.9.0/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.9.1/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
 
 $packageArgs = @{
@@ -10,9 +10,9 @@ $packageArgs = @{
     fileType       = 'exe'
     url64bit       = $url64
 
-    softwareName   = 'Ultimakercura*'
+    softwareName   = 'Ultimakercura'
 
-    checksum64     = '8b905dbe4264b45b31ff35d5196bd1d0faa9a24c251f5f22d5628adbf1ea4736'
+    checksum64     = 'f5a04999e5d09d57f4eaa93932b345932ed827b80cdc41d624dd0fc5e412d41a'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
@@ -22,3 +22,4 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 
 # https://github.com/Ultimaker/Cura/releases/download/4.9.0/Ultimaker_Cura-4.9.0-amd64.exe
+# https://software.ultimaker.com/cura/Ultimaker_Cura-4.9.1-amd64.exe
