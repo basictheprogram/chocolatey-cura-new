2a563b772e3f47b930aa16920309a727b2857e6c
Bob Tanner <tanner@real-time.com>
2021-07-05 22:18:56 -0500

    WIP: https://github.com/Ultimaker/Cura/issues/10091 keep the silent install silent
      * Install the Adafruit Industries certificate
      * Install the Arduino certificate

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$FullPackage = "Ultimaker_Cura-4.9.1-amd64.exe"
+
+$FullPackage = "Ultimaker_Cura-4.10.0-amd64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.9.1/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.10.0/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
+
+$ardunio = Join-Path $toolsDir 'Arduino.cer'
+$adafruit = Join-Path $toolsDir 'Adafruit.cer'
+
+Import-Certificate -FilePath $ardunio -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+Import-Certificate -FilePath $adafruit -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
@@ -12,7 +19,7 @@ $packageArgs = @{
 
     softwareName   = 'Ultimakercura'
 
-    checksum64     = 'f5a04999e5d09d57f4eaa93932b345932ed827b80cdc41d624dd0fc5e412d41a'
+    checksum64     = '001b2b412804ef1a10b00453ba422d1526a31844048bca23574d4116f688d98c'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
@@ -21,5 +28,4 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 
-# https://github.com/Ultimaker/Cura/releases/download/4.9.0/Ultimaker_Cura-4.9.0-amd64.exe
-# https://software.ultimaker.com/cura/Ultimaker_Cura-4.9.1-amd64.exe
+# https://github.com/Ultimaker/Cura/releases/download/4.10.0/Ultimaker_Cura-4.10.0-amd64.exe
