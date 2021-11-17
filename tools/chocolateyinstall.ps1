f2965fa277f081bbaf1cbfcc0e573d7525b5d2aa
Bob Tanner <tanner@real-time.com>
2021-11-17 12:35:26 -0600

    Closes #10: 4.12.0
      * New release cura-new v4.12.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$FullPackage = "Ultimaker_Cura-4.11.0-amd64.exe"
+$FullPackage = "Ultimaker_Cura-4.12.0-amd64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.11.0/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.12.0/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
 
 $ardunio = Join-Path $toolsDir 'Arduino.cer'
@@ -19,7 +19,7 @@ $packageArgs = @{
 
     softwareName   = 'Ultimakercura'
 
-    checksum64     = '0773d5225095e0b1b0c55c185eb29063abf42976bd49f3f74feea5f9ff7cd7f9'
+    checksum64     = 'ca5642735f3878bcf7e40b68fe977e17f8411f2d7bdd732bf72388475a87cc4e'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
