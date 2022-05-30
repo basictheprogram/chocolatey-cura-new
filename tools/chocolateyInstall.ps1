d9779072088682b9e2ccb4bc340ee49984766389
Bob Tanner <tanner@real-time.com>
2022-05-30 12:29:39 -0500

    Camel case for the install and uninstall scripts First attempt at a new release cura-new v5.0.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$FullPackage = "Ultimaker_Cura-4.13.1-amd64.exe"
+# https://github.com/Ultimaker/Cura/releases/download/5.0.0/Ultimaker-Cura-5.0.0-win64.exe
+
+$FullPackage = "Ultimaker-Cura-5.0.0-win64.exe"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Ultimaker/Cura/releases/download/4.13.1/' + $FullPackage
+$url64 = 'https://github.com/Ultimaker/Cura/releases/download/5.0.0/' + $FullPackage
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
 
 $ardunio = Join-Path $toolsDir 'Arduino.cer'
@@ -11,7 +13,7 @@ $adafruit = Join-Path $toolsDir 'Adafruit.cer'
 Import-Certificate -FilePath $ardunio -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 Import-Certificate -FilePath $adafruit -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
-& "$toolsDir\chocolateyuninstall.ps1"
+& "$toolsDir\chocolateyUninstall.ps1"
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
@@ -21,7 +23,7 @@ $packageArgs = @{
 
     softwareName   = 'Ultimaker'
 
-    checksum64     = '8de0c18a61a29cac9cbcd5b6d1db1de925f3ea36d67d50e0aabef81d66a91538'
+    checksum64     = '3adc5f24a0ae705a3e302587ed7950f05d6f2c60834fce13fa69c2aef3a8da62'
     checksumType64 = 'sha256'
 
     validExitCodes = @(0, 3010, 1641)
