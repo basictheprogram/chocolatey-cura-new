f9215b50f55d2ec376773abbc21c037c98e66820
junland <unlandj2012@gamil.com>
2022-10-24 17:29:41 -0500

    Create Cura 5.2.1

---
+++ tools/data.ps1
@@ -0,0 +1,5 @@
+@{
+    url = 'https://github.com/Ultimaker/Cura/releases/download/5.2.1/Ultimaker-Cura-5.2.1-win64.exe'
+    checksum = '9917cb0831ea750f691421ec62dd17c99d44b71e4a3a7290c438ed59e6a7c8ae'
+    checksumType = 'sha256'
+}
